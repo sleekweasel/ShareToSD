455e80e1bd96472a9a27703d9cf3c50ecefef128
SleekWeasel <sleekweasel@gmail.com>
2013-11-01 22:00:46 +0000

    Fix timestamp/date ordering bug

--- ShareToSD/src/main/java/uk/org/baverstock/sharetosd/DownloadEventsContract.java
+++ ShareToSD/src/main/java/uk/org/baverstock/sharetosd/DownloadEventsContract.java
@@ -7,7 +7,8 @@ public class DownloadEventsContract {
     public static final String EVENT_KEY_URL = "url";
     public static final String EVENT_KEY_TIMESTAMP_QUA_TIMESTAMP = "timestamp";
     public static final String EVENT_KEY_TIMESTAMP_QUA_MILLIS = "timestamp_ms";
-    public static final String EVENT_KEY_TIMESTAMP_INTO_DATE_MILLIS = "(strftime('%s', " + EVENT_KEY_TIMESTAMP_QUA_TIMESTAMP + ") * 1000) AS " + EVENT_KEY_TIMESTAMP_QUA_MILLIS;
+    public static final String EVENT_KEY_TIMESTAMP_INTO_DATE_MILLIS =
+            "(strftime('%s', " + EVENT_KEY_TIMESTAMP_QUA_TIMESTAMP + ") * 1000) AS " + EVENT_KEY_TIMESTAMP_QUA_MILLIS;
     public static final String EVENT_KEY_FILE = "file";
     public static final String EVENT_KEY_TYPE = "type";
     public static final String EVENT_TABLE = "event";
